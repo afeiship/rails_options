157b32bb0b6d8ba91e4424e16eed72c726618671
afeiship <1290657123@qq.com>
2022-03-17 08:23:32 +0800

    fix: rm text field

--- db/migrate/20220316133202_create_rails_options.rb
+++ db/migrate/20220316133202_create_rails_options.rb
@@ -3,7 +3,6 @@ class CreateRailsOptions < ActiveRecord::Migration[6.1]
     create_table :rails_options do |t|
       t.string :slug
       t.string :value
-      t.text :content
 
       t.timestamps
     end
--- lib/rails_options/version.rb
+++ lib/rails_options/version.rb
@@ -1,3 +1,3 @@
 module RailsOptions
-  VERSION = "0.1.4"
+  VERSION = "0.1.5"
 end
