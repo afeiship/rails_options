34f387f5e979a21cdba4c22ba68666e74432790b
afeiship <1290657123@qq.com>
2022-03-17 08:44:33 +0800

    fix: string to text

--- db/migrate/20220316133202_create_rails_options.rb
+++ db/migrate/20220316133202_create_rails_options.rb
@@ -2,7 +2,7 @@ class CreateRailsOptions < ActiveRecord::Migration[6.1]
   def change
     create_table :rails_options do |t|
       t.string :slug
-      t.string :value
+      t.text :value
 
       t.timestamps
     end
